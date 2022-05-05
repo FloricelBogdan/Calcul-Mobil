d7a61d28a5252b871f5e08695edf780abe0c6565
FloricelBogdan <65444693+FloricelBogdan@users.noreply.github.com>
2022-05-05 14:14:19 +0300

    First Commit

---
+++ app/src/main/java/com/willteksoftwares/bda/Email/Util.java
@@ -0,0 +1,9 @@
+package com.willteksoftwares.bda.Email;
+
+public class Util {
+
+    public static final String EMAIL = "blooddonationmobileapp@outlook.com";
+    public static final String PASSWORD = "mamasita99";
+
+
+}
